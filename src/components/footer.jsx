6177678829e2603a1200c21e708a973503e416ef
Krishna Gupta <krishnaaa@Krishnas-MacBook-Air.local>
2024-07-07 18:55:38 +0530

    footer added

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,25 @@
 import '../styles/footer.css'
+import { ReactComponent as Heart } from '../assets/heart-svgrepo-com.svg';
 
 function footer() {
-    return(
+    return (
         <div className="footer">
-            <div className='footer-name'>Krishna Gupta</div>
-            <div></div>
-            <div></div>
+            <div className='ft-name'>Krishna Gupta</div>
+            <div className='ft-items'>
+                <div className='work'>Work</div>
+                <div className='abt'>About</div>
+                <div className='ts'>Tech-Stack</div>
+                <div className='connect'>Connect</div>
+            </div>
+
+            <div className='love'>Made with
+                <span className='heart'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -6 24 24" width="24" height="30">
+                        <Heart width="24" height="24" />
+                    </svg>
+                </span>
+                by Krishna Gupta.
+            </div>
         </div>
     )
 }
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -26,31 +26,33 @@ function About() {
                 </div>
                 <div className='beliefs'>
                     <div className='outer-box'>
-                        <div className="inner-box-1">Make it</div>
-                        <div className="inner-box-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloribus nostrum eaque soluta et facilis, sit nesciunt asperiores aspernatur non dolorem tempore esse quos cumque voluptates dicta rerum, atque hic.</div>
+                        <div className="inner-box-1">Do it.</div>
+                        <div className="inner-box-2">I believe in starting instead of thinking, and finishing what I've started. Cutting corners is not something I believe in.</div>
                     </div>
                     <div className='outer-box'>
-                        <div className="inner-box-1">Experiment</div>
-                        <div className="inner-box-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloribus nostrum eaque soluta et facilis, sit nesciunt asperiores aspernatur non dolorem tempore esse quos cumque voluptates dicta rerum, atque hic.</div>
+                        <div className="inner-box-1">Experiment.</div>
+                        <div className="inner-box-2">A person should never be afraid of getting their hands on something new, and I believe I am one of that kind of a person.</div>
                     </div>
                     <div className='outer-box'>
-                        <div className="inner-box-1">Lorem ipsum</div>
-                        <div className="inner-box-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloribus nostrum eaque soluta et facilis, sit nesciunt asperiores aspernatur non dolorem tempore esse quos cumque voluptates dicta rerum, atque hic.</div>
+                        <div className="inner-box-1">Discuss.</div>
+                        <div className="inner-box-2">Desirable end results are not achieved in vacuum. I look forward to discuss, collaborate and work together on any kind of task.</div>
                     </div>
                     <div className='outer-box'>
-                        <div className="inner-box-1">Lorem Ipsum</div>
-                        <div className="inner-box-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloribus nostrum eaque soluta et facilis, sit nesciunt asperiores aspernatur non dolorem tempore esse quos cumque voluptates dicta rerum, atque hic.</div>
+                        <div className="inner-box-1">Fail? Never.</div>
+                        <div className="inner-box-2">I don't believe in failures. Setbacks always have a way to be overcame.</div>
                     </div>
                 </div>
             </div>
             <div className="me">
                 <div className='extra-text'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloribus nostrum eaque soluta et facilis, sit nesciunt asperiores aspernatur non dolorem tempore esse quos cumque voluptates dicta rerum, atque hic.
-                In et tempore voluptatem, nihil eaque ab sapiente eius itaque libero odit qui perspiciatis natus nostrum veniam obcaecati alias animi ex omnis unde dicta, incidunt nisi sit possimus laboriosam. Consectetur.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloribus nostrum eaque soluta et facilis, sit nesciunt asperiores aspernatur non dolorem tempore esse quos cumque voluptates dicta rerum, atque hic.
+                When you don't see me working, you can see my sketching, enjoying my time listening to good music, playing football or exploring around.
+                There's something called Life, which is not just restricted to your work. Everything else is just as much important for a person to grow.
+                Capturing beautiful moments is one of my many passions, embracing the surrounding will always be one of them.
                 </div>
                 <div className='photos'>
-
+                    <div className='sketch-1'></div>
+                    <div className='marine-drive'></div>
+                    <div className='sketch-2'></div>
                 </div>
             </div>
         <Footer />
