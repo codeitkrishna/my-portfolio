188262e4bc789d456b6f2902e64fd1e6ad268812
Krishna Gupta <krishnaaa@Krishnas-MacBook-Air.local>
2024-07-07 16:43:15 +0530

    Connect Page, Tech-stack page, background gradient

--- src/App.js
+++ src/App.js
@@ -13,6 +13,22 @@ function App() {
   return (
     <Router>
     <main className="base-app">
+    <div className="uiverse">
+      <div className="wrapper">
+        <div className="circle circle-12"></div>
+        <div className="circle circle-11"></div>
+        <div className="circle circle-10"></div>
+        <div className="circle circle-9"></div>
+        <div className="circle circle-8"></div>
+        <div className="circle circle-7"></div>
+        <div className="circle circle-6"></div>
+        <div className="circle circle-5"></div>
+        <div className="circle circle-4"></div>
+        <div className="circle circle-3"></div>
+        <div className="circle circle-2"></div>
+        <div className="circle circle-1"></div>
+        </div>
+      </div>
       <Navbar />
       <Routes>
         <Route path="/" element={<Work />} />
