cbdeba1577efe47404eaa6216facae1cde851606
Krishna Gupta <krishnaaa@Krishnas-MacBook-Air.local>
2024-07-08 19:40:01 +0530

    testing improvised code

--- src/pages/techStack.jsx
+++ src/pages/techStack.jsx
@@ -4,16 +4,16 @@ import Footer from '../components/footer';
 
 function TechStack() {
     const techStack = [
-        { name: 'Javascript', src: '/static/media/logos/8.svg' },
-        { name: 'TypeScript', src: '/static/media/logos/5.svg' },
-        { name: 'React', src: '/static/media/logos/2.svg' },
-        { name: 'Tailwind CSS', src: '/static/media/logos/9.svg' },
-        { name: 'NodeJS', src: '/static/media/logos/10.svg' },
-        { name: 'Mongo', src: '/static/media/logos/3.svg' },
-        { name: 'MySQL', src: '/static/media/logos/7.svg' },
-        { name: 'Figma', src: '/static/media/logos/4.svg' },
-        { name: 'C++', src: '/static/media/logos/6.svg' }, 
-        { name: 'Python', src: '/static/media/logos/11.svg' },
+        { name: 'Javascript', src: '/src/assets/logos/8.svg' },
+        { name: 'TypeScript', src: '/src/assets/logos/5.svg' },
+        { name: 'React', src: '/src/assets/logos/2.svg' },
+        { name: 'Tailwind CSS', src: '/src/assets/logos/9.svg' },
+        { name: 'NodeJS', src: '/src/assets/logos/10.svg' },
+        { name: 'Mongo', src: '/src/assets/logos/3.svg' },
+        { name: 'MySQL', src: '/src/assets/logos/7.svg' },
+        { name: 'Figma', src: '/src/assets/logos/4.svg' },
+        { name: 'C++', src: '/src/assets//logos/6.svg' }, 
+        { name: 'Python', src: '/src/assets/logos/11.svg' },
     ];
 
     return (
